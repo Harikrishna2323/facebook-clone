e3bdfcdfbfc891726bea87a847508809cc51ec57
Harikrishna2323 <harikb23@gmail.com>
2024-05-22 23:25:00 +0530

    initial commit

---
+++ src/pages/login/Login.js
@@ -0,0 +1,17 @@
+import "./login.css";
+import LoginForm from "../../components/login/LoginForm";
+import Footer from "../../components/login/Footer";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <LoginForm />
+        <div className="register"></div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
